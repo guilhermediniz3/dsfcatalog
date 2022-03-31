54173be803f4ee5f0576e3f1bf514aa7c3b3b540
guilhermediniz3 <gdiniztec@yahoo.com.br>
2022-03-31 17:40:08 -0300

    endpoits finalizados

--- src/main/java/com/devsuperior/dsfcatalog/services/ClientService.java
+++ src/main/java/com/devsuperior/dsfcatalog/services/ClientService.java
@@ -4,7 +4,11 @@ package com.devsuperior.dsfcatalog.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dsfcatalog.dto.ClientDTO;
 import com.devsuperior.dsfcatalog.entities.Client;
 import com.devsuperior.dsfcatalog.repositories.ClientRepository;
+import com.devsuperior.dsfcatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dsfcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,4 +36,55 @@ public class ClientService {
 	Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not Found"));
 	return new ClientDTO(entity);
 	}
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+	try {
+		Client entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+		
+	}
+	catch(EntityNotFoundException e){
+		throw new ResourceNotFoundException("id not found" + "-" + id);
+		
+	}
+	}
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		
+		catch (EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException("id not foud" + "-"+ id);
+			
+		}
+		catch(DataIntegrityViolationException e) {
+			
+			throw new DatabaseException("integrrity violation Client já possui vinculos");
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
 }
