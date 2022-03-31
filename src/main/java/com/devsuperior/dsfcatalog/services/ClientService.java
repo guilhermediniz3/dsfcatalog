ef34afc2f7da980156b6194c20e1c4c3adbe5828
guilhermediniz3 <gdiniztec@yahoo.com.br>
2022-03-31 16:29:01 -0300

    exceptions e endpont buscando por id

--- src/main/java/com/devsuperior/dsfcatalog/services/ClientService.java
+++ src/main/java/com/devsuperior/dsfcatalog/services/ClientService.java
@@ -1,24 +1,34 @@
 package com.devsuperior.dsfcatalog.services;
 
-import java.util.List;
+
+
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsfcatalog.dto.ClientDTO;
 import com.devsuperior.dsfcatalog.entities.Client;
 import com.devsuperior.dsfcatalog.repositories.ClientRepository;
+import com.devsuperior.dsfcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
 	@Autowired
 	private ClientRepository repository;
-	
+	@Transactional(readOnly =true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 	Page<Client> list = repository.findAll(pageRequest);
 	
 		return list.map(x -> new ClientDTO(x));
+	}
+	@Transactional(readOnly =true)
+	public ClientDTO findById(Long id) {
+	Optional<Client> obj = repository.findById(id);
+	Client entity = obj.orElseThrow(()-> new ResourceNotFoundException("Entity not Found"));
+	return new ClientDTO(entity);
 	}
 }
